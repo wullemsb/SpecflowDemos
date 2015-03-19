c4d9ebc4563bcf8f8a3bb6fb200cf6b424782545
wullemsb <wullemsb@gmail.com>
2015-03-19 12:20:28 +0100

    Add some test hooks

---
+++ SpecFlowDemos/Hooks1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowDemos
+{
+    [Binding]
+    public class Hooks1
+    {
+        // For additional details on SpecFlow hooks see http://go.specflow.org/doc-hooks
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            Trace.WriteLine("Before scenario");
+        }
+
+        [BeforeStep]
+        public void BeforeStep()
+        {
+            Trace.WriteLine("Before step");
+        }
+
+        [AfterStep]
+        public void AfterStep()
+        {
+            Trace.WriteLine("After step");
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            Trace.WriteLine("After scenario");
+        }
+    }
+}
